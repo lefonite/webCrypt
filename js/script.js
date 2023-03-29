4ab7606dc8add44739cb27b5683952749844604d
le-fonite <lefonite@gmail.com>
2023-03-29 12:38:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+function openPage(pageName, elmnt, color) {
+	// Hide all elements with class="tabcontent" by default */
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Remove the background color of all tablinks/buttons
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].style.backgroundColor = "";
+	}
+  
+	// Show the specific tab content
+	document.getElementById(pageName).style.display = "block";
+  
+	// Add the specific color to the button used to open the tab content
+	elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
+
+
+
